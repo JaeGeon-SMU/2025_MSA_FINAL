d4282609d22a5b1c846b693e4ce796bf60ab6709
JaeGeon-SMU <dlworjs1021@gmail.com>
2025-10-30 09:42:11 +0900

    project init

---
+++ popupplatform/src/main/java/com/popspot/popupplatform/PopupplatformApplication.java
@@ -0,0 +1,13 @@
+package com.popspot.popupplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopupplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopupplatformApplication.class, args);
+	}
+
+}
---
+++ popupplatform/src/test/java/com/popspot/popupplatform/PopupplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popspot.popupplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopupplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
